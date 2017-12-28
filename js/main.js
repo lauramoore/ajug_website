53890bc7c17db95ea3f8b49b693a83da70d9b24d
Rett Walker <rett@warplabs.io>
2017-12-27 21:28:24 -0500

    update changes to team and add min-height

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 
 (function($) {
     $(document).ready(function(){
-      $( ".meet-team-btn" ).click(function( event ) {
-        event.preventDefault();
-        $('body').addClass('noScroll');
-        $('#about').addClass('hidden');
-        $('#team-page').addClass('not-hidden slideInLeft');
-      });
+      // $( ".meet-team-btn" ).click(function( event ) {
+      //   event.preventDefault();
+      //   $('body').addClass('noScroll');
+      //   $('#about').addClass('hidden');
+      //   $('#team-page').addClass('not-hidden slideInLeft');
+      // });
 
       $('.team-info').on('hide.bs.collapse', function () {
         $('#'+this.id+'Btn.arrow-button').removeClass('oi-caret-bottom');

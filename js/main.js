f5ace5fcf085f42481b31f5facac272251e46b46
Unknown <rett@warplabs.io>
2018-01-02 23:34:02 -0500

    set background on normal ipad

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@
       if(screen.width === 1366 && screen.height === 1024){
         $('.parallax-window').attr('data-image-src','/assets/img/background/a-ipad-side-v7.svg');
       }
+
+      if(screen.width === 1024 && screen.height === 768){
+        $('.parallax-window').attr('data-image-src','/assets/img/background/a-ipad-side-v7.svg');
+      }
+
+
       $( ".meet-team-btn" ).click(function( event ) {
         event.preventDefault();
         $('.team-modal').modal('toggle')

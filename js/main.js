483638be890b721d5ffc7b82a3468fc70d4b6042
Unknown <rett@warplabs.io>
2018-01-11 22:01:11 -0500

    changes to layout and sizing of some text

--- js/main.js
+++ js/main.js
@@ -2,16 +2,22 @@
 (function($) {
     $(document).ready(function(){
 
-      if((screen.width === 768 && screen.height === 1024) || (screen.width === 1024 && screen.height === 768)
-        || (screen.height === 1366 && screen.width === 1024) || (screen.height === 1024 && screen.width === 1366)){
+      if((screen.width === 1024 && screen.height === 768)|| (screen.height === 1366 && screen.width === 1024)){
         $('.parallax-window').attr('data-image-src','/assets/img/background/a-ipad-1.svg');
       }
+      // 
+      // if((screen.height === 1024 && screen.width === 1366)||(screen.width === 768 && screen.height === 1024) ){
+      //   $('.parallax-window').attr('data-image-src','/assets/img/background/a-ipad.svg');
+      // }
 
       if(screen.width <= 768){
-        $('.parallax-window').removeAttr('data-image-src').removeAttr('data-parallax').removeClass('parallax-window');
         $('.ajug_becomemember_banner').attr('src', '/assets/img/ajug_becomemember_banner_rect.png');
       }
-
+  
+      if($( window ).width() <= 1199){
+        $('.parallax-window').removeAttr('data-image-src').removeAttr('data-parallax').removeClass('parallax-window');
+      }
+      
       $( window ).resize(function() {
         if($( window ).width() <= 768){
           //$('.parallax-window').removeAttr('data-image-src').removeAttr('data-parallax').removeClass('parallax-window');
@@ -19,8 +25,7 @@
         } else {
           $('.ajug_becomemember_banner').attr('src', '/assets/img/ajug_becomemember_banner.png');
         }
-
-
+        
       });
       $( ".meet-team-btn" ).click(function( event ) {
         event.preventDefault();
